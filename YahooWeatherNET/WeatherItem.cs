5f62a2934c2d4872903170fd1eda7a50a0e19d19
Marco Stambor <mstambor@googlemail.com>
2016-03-28 18:27:01 +0200

    Initial commit

---
+++ YahooWeatherNET/WeatherItem.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace YahooWeatherNET
+{
+    /// <summary>
+    /// Contains the requested weather report
+    /// </summary>
+    public class WeatherItem
+    {
+        /// <summary>
+        /// Information about the location the forecast is determined to
+        /// </summary>
+        public WeatherLocation WLocation { get; set; }
+        /// <summary>
+        /// Information about the measurement units used
+        /// </summary>
+        public MeasurementUnits Units { get; set; }
+        /// <summary>
+        /// Information about the wind conditions
+        /// </summary>
+        public WindConditions Wind { get; set; }
+        /// <summary>
+        /// Information about the atmosphere conditions
+        /// </summary>
+        public AtmosphereConditions Atmosphere { get; set; }
+        /// <summary>
+        /// Information about astronomic conditions
+        /// </summary>
+        public AstronomyInfo Astronomy { get; set; }
+        /// <summary>
+        /// Title of weather report, e.g. 'Conditions for Koeln, DE at 6:00 pm CET'
+        /// </summary>
+        public string Title { get; set; }
+        /// <summary>
+        /// Exact position of the location the forecast is determined to
+        /// </summary>
+        public GeoLocation Location { get; set; }
+        /// <summary>
+        /// Link to online version
+        /// </summary>
+        public string Link { get; set; }
+        /// <summary>
+        /// Date of when the forecast was made
+        /// </summary>
+        public string Published { get; set; }
+        /// <summary>
+        /// Information about current weather conditions
+        /// </summary>
+        public WeatherCondition CurrentCondition { get; set; }
+        /// <summary>
+        /// Text version of weather report, formatted in HTML
+        /// </summary>
+        public string Description { get; set; }
+        /// <summary>
+        /// List of forecasts. Forecast for today is element 0, forecast for tomorrow is element 1
+        /// </summary>
+        public List<WeatherForecast> Forecasts { get; set; }
+    }
+    /// <summary>
+    /// Contains information about sunrise and sunset
+    /// </summary>
+    public class AstronomyInfo
+    {
+        /// <summary>
+        /// Time of sunrise [hh:mm pm]
+        /// </summary>
+        public string Sunrise { get; set; }
+        /// <summary>
+        /// Time of sunset [hh:mm am]
+        /// </summary>
+        public string Sunset { get; set; }
+    }
+    /// <summary>
+    /// Detailed information about atmosphere conditions
+    /// </summary>
+    public class AtmosphereConditions
+    {
+        /// <summary>
+        /// Humidity in percent
+        /// </summary>
+        public string Humidity { get; set; }
+        /// <summary>
+        /// Multiply with 100 to get visibility in miles or km (depends on Units)
+        /// </summary>
+        public string Visibility { get; set; }
+        /// <summary>
+        /// Air pressure in 'in' or 'mb' (depends on Units)
+        /// </summary>
+        public string Pressure { get; set; }
+        /// <summary>
+        /// Trend of air pressure. 0 = steady, 1 = rising, 2 = falling
+        /// </summary>
+        public string Rising { get; set; }
+    }
+    /// <summary>
+    /// Detailed information about wind conditions
+    /// </summary>
+    public class WindConditions
+    {
+        /// <summary>
+        /// Windchill in degrees (depends on Units)
+        /// </summary>
+        public string Chill { get; set; }
+        /// <summary>
+        /// Direction of wind in degrees
+        /// </summary>
+        public string Direction { get; set; }
+        /// <summary>
+        /// Windspeed in mph or kph (depends on Units)
+        /// </summary>
+        public string Speed { get; set; }
+    }
+    /// <summary>
+    /// Further information about the location the forecast is determined to
+    /// </summary>
+    public class WeatherLocation
+    {
+        public string City { get; set; }
+        /// <summary>
+        /// Region-code made of 2 characters, if available
+        /// </summary>
+        public string Region { get; set; }
+        public string Country { get; set; }
+    }
+    /// <summary>
+    /// Units for forecast information
+    /// </summary>
+    public class MeasurementUnits
+    {
+        /// <summary>
+        /// Degree, Celsius (c) or Fahrenheit (f)
+        /// </summary>
+        public string Temperature { get; set; }
+        /// <summary>
+        /// miles (m) or kilometer (km)
+        /// </summary>
+        public string Distance { get; set; }
+        /// <summary>
+        /// pounds per square inch (in) or millibars (mb)
+        /// </summary>
+        public string Pressure { get; set; }
+        /// <summary>
+        /// miles per hour (mph) or kilometers per hour (kph)
+        /// </summary>
+        public string Speed { get; set; }
+    }
+    /// <summary>
+    /// Current weather conditions
+    /// </summary>
+    public class WeatherCondition
+    {
+        /// <summary>
+        /// Description of weather conditions
+        /// </summary>
+        public string Text { get; set; }
+        /// <summary>
+        /// Weather condition code (see http://developer.yahoo.com/weather/#codes )
+        /// </summary>
+        public string Code { get; set; }
+        /// <summary>
+        /// Current temperature
+        /// </summary>
+        public string Temperature { get; set; }
+        /// <summary>
+        /// Exact date and time te forecast was made, in RFC822 Section 5 format
+        /// </summary>
+        public string Date { get; set; }
+    }
+    /// <summary>
+    /// Contains information about weather forecast
+    /// </summary>
+    public class WeatherForecast
+    {
+        /// <summary>
+        /// Day of Week the forecast is made for
+        /// </summary>
+        public string Day { get; set; }
+        /// <summary>
+        /// The exact Date [dd Mmm yyyy]
+        /// </summary>
+        public string Date { get; set; }
+        /// <summary>
+        /// Max temperature announced
+        /// </summary>
+        public string HighTemperature { get; set; }
+        /// <summary>
+        /// Min temperature announced
+        /// </summary>
+        public string LowTemperature { get; set; }
+        /// <summary>
+        /// Description of weather conditions
+        /// </summary>
+        public string Text { get; set; }
+        /// <summary>
+        /// Weather condition code (see http://developer.yahoo.com/weather/#codes )
+        /// </summary>
+        public string Code { get; set; }
+    }
+    /// <summary>
+    /// Exact position of location
+    /// </summary>
+    public class GeoLocation
+    {
+        public string Lat{ get; set; }
+        public string Long { get; set; }
+    }
+}
---
+++ YahooWeatherNET/YahooWeatherProvider.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Xml.Linq;
+using System.Xml;
+using System.Text;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace YahooWeatherNET
+{
+    public class YahooWeatherProvider
+    {
+        private string m_consumerKey;
+        private string m_consumerSecret;
+        private string m_weatherCode;
+        private string m_temperatureUnit;
+        private static readonly XNamespace yweather = "http://xml.weather.yahoo.com/ns/rss/1.0";
+        private static readonly XNamespace geo = "http://www.w3.org/2003/01/geo/wgs84_pos#";
+        private static readonly XNamespace xmlns = "http://where.yahooapis.com/v1/schema.rng";
+
+        /// <summary>
+        /// Provides possibility to gather weather information from yahoo.com
+        /// </summary>
+        /// <param name="_weatherCode">WOEID which indicates the _location for the weather forecast</param>
+        /// <param name="_tempUnit">The units which should be used (Celsius includes metric system)</param>
+        public YahooWeatherProvider(string _weatherCode, TemperatureUnit _tempUnit, string _consumerKey, string _consumerSecret)
+        {
+            m_consumerKey = _consumerKey;
+            m_consumerSecret = _consumerSecret;
+            m_weatherCode = _weatherCode;
+            if (_tempUnit == TemperatureUnit.Celsius)
+            {
+                m_temperatureUnit = "c";
+            }
+            else
+            {
+                m_temperatureUnit = "f";
+            }
+        }
+
+        /// <summary>
+        /// Resolve the WOEID (Weather Code) from a search term (POI, City, weather station)
+        /// </summary>
+        /// <param name="_location">search term (e.g. 'Cologne DE', 'Dusseldorf', 'Kölner Dom')</param>
+        /// <returns></returns>
+        public static string ResolveWeatherCode(string _location)
+        {
+            WebClient mWebClient = new WebClient();
+            string loc = mWebClient.DownloadString("http://where.yahooapis.com/v1/places.q('" + _location + "')?appid=jVMO1MvV34GYE4aEqO7ZTnAKuCpPU0Adp5kiAb1oxhzcFmjRhzj6h85eRL31Q3n9tA3.4es7B9Ep1I0tAEfx.qjLqSxJiJM-");
+            byte[] bytes = System.Text.Encoding.ASCII.GetBytes(loc);
+            MemoryStream stream = new MemoryStream(bytes);
+            XDocument _XDocument = XDocument.Load(stream);
+            string woeid;
+            try
+            {
+                woeid = (from x in _XDocument.Descendants(xmlns + "places").Descendants(xmlns + "place").Descendants(xmlns + "woeid") select x.Value).First();
+            }
+            catch (InvalidOperationException ex)
+            {
+                woeid = "0";
+            }
+            return woeid;
+        }
+
+        /// <summary>
+        /// Requests a new weather forecast. If the Weather Code provided is invalid, the Title property of the returned WeatherItem will be set to 'City not found'
+        /// </summary>
+        /// <returns></returns>
+        public WeatherItem GetWeather() 
+        {
+            WebClient client = new WebClient();
+            client.Headers.Add("Authorization: OAuth " +
+                "realm=\"yahooapis.com\","+
+                "oauth_consumer_key=\"" + m_consumerKey + "\","+
+                "oauth_nonce=\"" + Guid.NewGuid().ToString() + "\","+
+                "oauth_signature_method=\"PLAINTEXT\","+
+                "oauth_timestamp=\"" + ((DateTime.UtcNow.Ticks - new DateTime(1970, 1, 1).Ticks) / (1000 * 10000)).ToString() + "\"," +
+                "oauth_version=\"1.0\"," +
+                "oauth_signature=\"" + m_consumerSecret + "%26\","+
+                "oauth_callback=\"oob\"");
+
+            string rawForecast = client.DownloadString("http://weather.yahooapis.com/forecastrss?w=" + m_weatherCode + "&u=" + m_temperatureUnit);
+
+            return ParseReport(rawForecast);
+        }
+
+        /// <summary>
+        /// Requests a new weather forecast asynchronously. If the Weather Code provided is invalid, the Title property of the returned WeatherItem will be set to 'City not found'
+        /// </summary>
+        /// <returns></returns>
+        public async Task<WeatherItem> GetWeatherAsync()
+        {
+            string query = "select * from weather.forecast where woeid=" + m_weatherCode;
+
+            WebClient client = new WebClient();
+            client.Headers.Add("Authorization: OAuth " +
+                "realm=\"yahooapis.com\"," +
+                "oauth_consumer_key=\"" + m_consumerKey + "\"," +
+                "oauth_nonce=\"" + Guid.NewGuid().ToString() + "\"," +
+                "oauth_signature_method=\"PLAINTEXT\"," +
+                "oauth_timestamp=\"" + ((DateTime.UtcNow.Ticks - new DateTime(1970, 1, 1).Ticks) / (1000 * 10000)).ToString() + "\"," +
+                "oauth_version=\"1.0\"," +
+                "oauth_signature=\"" + m_consumerSecret + "%26\"," +
+                "oauth_callback=\"oob\"");
+
+            try
+            {
+                string rawForecast =
+                    await
+                        client.DownloadStringTaskAsync(
+                            new Uri("http://weather.yahooapis.com/forecastrss?w=" + m_weatherCode + "&u=" +
+                                    m_temperatureUnit));
+                return ParseReport(rawForecast);
+            }
+            catch (WebException _ex)
+            {
+                return null;
+            }
+
+        }
+
+        /// <summary>
+        /// Parses the report received from the weather API
+        /// </summary>
+        /// <param name="_forecast">The raw forecast data in XML</param>
+        /// <returns></returns>
+        private WeatherItem ParseReport(string _forecast)
+        {
+            byte[] bytes = System.Text.Encoding.ASCII.GetBytes(_forecast);
+            MemoryStream stream = new MemoryStream(bytes);
+            XDocument xDocument = XDocument.Load(stream);
+
+            WeatherItem weatherItem = new WeatherItem();
+
+            weatherItem.Title = (from x in xDocument.Descendants("channel").Descendants("item").Descendants("title") select x.Value).Single();
+
+            if (weatherItem.Title == "City not found") { return weatherItem; }
+
+            WeatherLocation weatherLocation = new WeatherLocation();
+            weatherLocation.City = GetValueFromYWeatherItem("location", "city", xDocument);
+            weatherLocation.Region = GetValueFromYWeatherItem("location", "region", xDocument);
+            weatherLocation.Country = GetValueFromYWeatherItem("location", "country", xDocument);
+            weatherItem.WLocation = weatherLocation;
+
+            MeasurementUnits units = new MeasurementUnits();
+            units.Distance = GetValueFromYWeatherItem("units", "distance", xDocument);
+            units.Pressure = GetValueFromYWeatherItem("units", "pressure", xDocument);
+            units.Speed = GetValueFromYWeatherItem("units", "speed", xDocument);
+            units.Temperature = GetValueFromYWeatherItem("units", "temperature", xDocument);
+            weatherItem.Units = units;
+
+            WindConditions wind = new WindConditions();
+            wind.Chill = GetValueFromYWeatherItem("wind", "chill", xDocument);
+            wind.Direction = GetValueFromYWeatherItem("wind", "direction", xDocument);
+            wind.Speed = GetValueFromYWeatherItem("wind", "speed", xDocument);
+            weatherItem.Wind = wind;
+
+            AtmosphereConditions atmosphere = new AtmosphereConditions();
+            atmosphere.Humidity = GetValueFromYWeatherItem("atmosphere", "humidity", xDocument);
+            atmosphere.Pressure = GetValueFromYWeatherItem("atmosphere", "pressure", xDocument);
+            atmosphere.Rising = GetValueFromYWeatherItem("atmosphere", "rising", xDocument);
+            atmosphere.Visibility = GetValueFromYWeatherItem("atmosphere", "visibility", xDocument);
+            weatherItem.Atmosphere = atmosphere;
+
+            AstronomyInfo astronomy = new AstronomyInfo();
+            astronomy.Sunrise = GetValueFromYWeatherItem("astronomy", "sunrise", xDocument);
+            astronomy.Sunset = GetValueFromYWeatherItem("astronomy", "sunset", xDocument);
+            weatherItem.Astronomy = astronomy;
+
+            GeoLocation loc = new GeoLocation();
+            loc.Lat = GetValueFromGeoItem("lat", xDocument);
+            loc.Long = GetValueFromGeoItem("long", xDocument);
+
+            weatherItem.Location = loc;
+            weatherItem.Link = (from x in xDocument.Descendants("channel").Descendants("item").Descendants("link") select x.Value).Single();
+            weatherItem.Published = (from x in xDocument.Descendants("channel").Descendants("item").Descendants("pubDate") select x.Value).Single();
+
+            List<WeatherForecast> forecasts = new List<WeatherForecast>();
+            var locForecast = from x in xDocument.Descendants("channel").Descendants("item").Descendants(yweather + "forecast") select x;
+            foreach (XElement el in locForecast)
+            {
+                WeatherForecast wfc = new WeatherForecast();
+                wfc.Code = el.Attribute("code").Value;
+                wfc.Date = el.Attribute("date").Value;
+                wfc.Day = el.Attribute("day").Value;
+                wfc.HighTemperature = el.Attribute("high").Value;
+                wfc.LowTemperature = el.Attribute("low").Value;
+                wfc.Text = el.Attribute("text").Value;
+                forecasts.Add(wfc);
+            }
+
+            weatherItem.Forecasts = forecasts;
+
+            return weatherItem;
+        }
+
+        private string GetValueFromYWeatherItem(string _descendantName, string _attributeName, XDocument _doc)
+        {
+            return (from x in _doc.Descendants(yweather + _descendantName) select x.Attribute(_attributeName).Value).Single();
+        }
+
+        private string GetValueFromGeoItem(string _descendantName, XDocument _doc)
+        {
+            return (from x in _doc.Descendants("channel").Descendants("item").Descendants(geo + _descendantName) select x.Value).Single();
+        }
+        
+    }
+
+    /// <summary>
+    /// Unit which should be used for requesting weather data
+    /// </summary>
+    public enum TemperatureUnit
+    {
+        Celsius = 0,
+        Fahrenheit = 1
+    }
+}
